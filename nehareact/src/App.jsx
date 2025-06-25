4974abf268f85149a2f8a3307b5c418a91b14ffb
Neha Yadav <ny663922@gmail.com>
2025-06-26 03:14:00 +0530

    external.css in react

--- nehareact/src/App.jsx
+++ nehareact/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Inline from "./inline-style";
 import Dynamic from "./dynamic-inline";
+import External from "./external-css";
 
 function App() {
 
   return (
     <div>
-      <Dynamic />
+      <External />
 
     </div>
   )
---
+++ nehareact/src/external-css.jsx
@@ -0,0 +1,111 @@
+import './css/style.css'
+import women3 from "./assets/images/women-3.jpg";
+function External() {
+
+    return (
+        <div>
+            <h1 className="heading">external css</h1>
+            <div className='cntainer'>
+                <div className='box'>
+                    <img className="image" src={women3} alt="women" />
+
+                    <div className='text-wrap '>
+                        <h3>Neha yadav</h3>
+                        <p>front end developer</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <img className="image" src={women3} alt="women" />
+
+                    <div className='text-wrap '>
+                        <h3>Neha yadav</h3>
+                        <p>front end developer</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <img className="image" src={women3} alt="women" />
+
+                    <div className='text-wrap '>
+                        <h3>Neha yadav</h3>
+                        <p>front end developer</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <img className="image" src={women3} alt="women" />
+
+                    <div className='text-wrap '>
+                        <h3>Neha yadav</h3>
+                        <p>front end developer</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <img className="image" src={women3} alt="women" />
+
+                    <div className='text-wrap '>
+                        <h3>Neha yadav</h3>
+                        <p>front end developer</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <img className="image" src={women3} alt="women" />
+
+                    <div className='text-wrap '>
+                        <h3>Neha yadav</h3>
+                        <p>front end developer</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <img className="image" src={women3} alt="women" />
+
+                    <div className='text-wrap '>
+                        <h3>Neha yadav</h3>
+                        <p>front end developer</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <img className="image" src={women3} alt="women" />
+
+                    <div className='text-wrap '>
+                        <h3>Neha yadav</h3>
+                        <p>front end developer</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <img className="image" src={women3} alt="women" />
+
+                    <div className='text-wrap '>
+                        <h3>Neha yadav</h3>
+                        <p>front end developer</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <img className="image" src={women3} alt="women" />
+
+                    <div className='text-wrap '>
+                        <h3>Neha yadav</h3>
+                        <p>front end developer</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <img className="image" src={women3} alt="women" />
+
+                    <div className='text-wrap '>
+                        <h3>Neha yadav</h3>
+                        <p>front end developer</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <img className="image" src={women3} alt="women" />
+
+                    <div className='text-wrap '>
+                        <h3>Neha yadav</h3>
+                        <p>front end developer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default External;
